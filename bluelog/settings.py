d4e582dfdb935b2295c019eab0dc2a107d2aef82
lipeng <lipeng@bailuntec.com>
2020-12-28 15:55:49 +0800

    update

--- bluelog/settings.py
+++ bluelog/settings.py
@@ -50,7 +50,7 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     # SQLALCHEMY_DATABASE_URI = prefix + os.path.join(basedir, 'data-dev.db')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:lp120077@106.52.108.17:4312/ihome'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@127.0.0.1:4312/ihome'
 
 
 class TestingConfig(BaseConfig):
@@ -61,7 +61,7 @@ class TestingConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', prefix + os.path.join(basedir, 'data.db'))
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:lp120077@106.52.108.17:4312/ihome'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:lp120077@127.0.0.1:4312/ihome'
 
 
 config = {
